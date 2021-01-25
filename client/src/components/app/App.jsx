a0bccb2eb17dbc45310372732a5e57576b427d7b
Lydia Goodson <lydiaggoodson@gmail.com>
2021-01-25 15:54:05 -0600

    Added state routing

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import styles from './app.module';
 
 class Navigation extends Component {
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import { join } from 'path';
 import express from 'express';
 import router from './routes/api';
+import stateRouting from './middleware/routing.mw';
 
 const CLIENT_PATH = join(__dirname, '../../client');
 require('dotenv').config()
@@ -11,6 +12,7 @@ app.use(express.static(CLIENT_PATH));
 app.use(express.json());
 
 app.use('/api', router);
+app.use(stateRouting);
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
